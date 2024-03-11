c417845e520182f357f52d84043fe32d446be623
BrookeSherman03 <112006636+BrookeSherman03@users.noreply.github.com>
2024-03-10 20:13:57 -0400

    Update feature_load.R

--- src/features/feature_load.R
+++ src/features/feature_load.R
@@ -10,33 +10,42 @@ train <- fread("./Midterm_Project/volume/data/raw/Stat_380_train.csv")
 test <- fread("./Midterm_Project/volume/data/raw/Stat_380_test.csv")
 covar <- fread("./Midterm_Project/volume/data/raw/covar_data.csv")
 
-#push data together to include covar
-id <- covar$sample_id
-newcovar <- subset(covar, select = -c(sample_id))
-total <- data.table(rowMeans(newcovar)) #use rowMeans to get average of each row
-total$sample_id <- id
+#prep for data to come together
+test$ic50_Omicron <- 0
+test$train <- 1
+train$train <- 0
 
-#merge data
-newtrain <- merge(x = train, y = total, by = 'sample_id')
-newtest <- merge(x = test, y = total, by = 'sample_id')
+#combine data
+master <- rbind(test, train)
 
-#keep ic50_omicron columns and testid column
-train_y <- newtrain$ic50_Omicron
-newtest$ic50_Omicron <- 0
-sample_id <- newtest$sample_id
-sample_id1 <- newtrain$sample_id
+#feature engineering
+master$days_sinceDose3 <- abs(master$days_sinceDose3)
+master$days_sincePosTest_latest <- abs(master$days_sincePosTest_latest)
+master$days_sinceSxLatest <- abs(master$days_sinceSxLatest)
+master$days_sinceDose3[is.na(master$days_sinceDose3)] <- mean(master$days_sinceDose3, na.rm = TRUE)
+master$Sx_severity_most_recent[is.na(master$Sx_severity_most_recent)] <- mean(master$Sx_severity_most_recent, na.rm = TRUE)
+master$days_dose23interval[is.na(master$days_dose23interval)] <- mean(master$days_dose23interval, na.rm = TRUE)
+master$days_sinceSxLatest[is.na(master$days_sinceSxLatest)] <- max(master$days_sinceSxLatest, na.rm = TRUE) + 1
+master$days_sincePosTest_latest[is.na(master$days_sincePosTest_latest)] <- mean(master$days_sincePosTest_latest, na.rm = TRUE)
 
-#prep for dummyVars
-newtrain <- subset(newtrain, select = -c(sample_id))
-newtest <- subset(newtest, select = -c(sample_id))
+#new column creation
+master$days_sinceDose1 <- abs((master$days_sinceDose2 - master$days_dose12interval))
+master$muldose3interval <- (master$days_dose23interval * master$days_sinceDose3)
+master$muldose2interval <- (master$days_dose12interval * master$days_sinceDose2)
+master$agemul_avginterval <- ((master$days_dose12interval + master$days_dose23interval)/2) * master$age
+master$Posmul_Sxlatest <- master$days_sinceSxLatest * master$days_sincePosTest_latest
+master$muldoseinterval <- (master$muldose2interval * master$muldose3interval)
+master$mulSx <- master$Sx_severity_most_recent * master$days_sinceSxLatest
+master$Agemuldays_sinceDoses <- master$age * (master$days_sinceDose1 + master$days_sinceDose2 + master$days_sinceDose3)
+master$posmuldays_sinceDoses <- master$days_sincePosTest_latest * (master$days_sinceDose1 + master$days_sinceDose2 + master$days_sinceDose3)
+master$Sxmuldays_sinceDoses <- master$mulSx * (master$days_sinceDose1 + master$days_sinceDose2 + master$days_sinceDose3)
 
-#fill all NA values in test and training tables
-newtrain[is.na(newtrain)] <- 0
-newtest[is.na(newtest)] <- 0
+#separate into train and test
+train <- master[train == 0]
+test <- master[train == 1]
 
-#readd sample_id
-newtest$sample_id <- sample_id
-newtrain$sample_id <- sample_id1
+train <- subset(train, select = -c(train))
+test <- subset(test, select = -c(train))
 
-fwrite(newtrain, "./Midterm_Project/volume/data/interim/newtrain.csv")
-fwrite(newtest, "./Midterm_Project/volume/data/interim/newtest.csv")
+fwrite(newtrain, "./Midterm_Project/volume/data/interim/train.csv")
+fwrite(newtest, "./Midterm_Project/volume/data/interim/test.csv")
